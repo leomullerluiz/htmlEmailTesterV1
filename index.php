3d242982a8a12ad6000047fef2bc83d5c78a2512
Leonan Müller <leomullerluiz@gmail.com>
2023-08-04 09:17:42 -0300

    dev actions

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require "app/functions/pages.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HTML Email Tester</title>
+    <title>[DEV] HTML Email Tester</title>
     <link rel="shortcut icon" href="pages/res/img/html_icon.svg" type="image/x-icon">
     <!-- FontAwesome -->
     <script src="https://kit.fontawesome.com/c75c18e7a6.js" crossorigin="anonymous"></script>
