06fbf7640218852d7502b78917c6249b278d4184
Leonan Müller <leomullerluiz@gmail.com>
2023-08-09 20:15:26 -0300

    sample of page load

--- app/functions/pages.php
+++ app/functions/pages.php
@@ -1,6 +1,7 @@
 <?php
 
     function loadPage(){
+        //SAMPLE: http://localhost:5000/?page=teste
         $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING);
         $page = (!$page) ? 'pages/home.php' : "pages/{$page}.php";
         if(!file_exists($page)){
